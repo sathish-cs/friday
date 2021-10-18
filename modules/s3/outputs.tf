20f9b2b08edb4d106d28b54f5d313ae58fbdccd0
root <root@dev.env>
2021-10-18 15:07:26 +0000

    Updated outputs

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,10 +1,10 @@
 output "Hourly_bucket_name" {
-    value = aws_s3_bucket.s3_bucket_hour.bucket
+  value = aws_s3_bucket.s3_bucket_hour.bucket
 }
 
 output "Weekly_bucket_name" {
-   value = aws_s3_bucket.s3_bucket_week.bucket
+  value = aws_s3_bucket.s3_bucket_week.bucket
 }
 output "Monthly_bucket_name" {
-   value = aws_s3_bucket.s3_bucket_month.bucket
+  value = aws_s3_bucket.s3_bucket_month.bucket
 }
