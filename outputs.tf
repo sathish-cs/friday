0d969a7ac87b1d33fb80917840d170d8ad79c686
root <root@dev.env>
2021-10-18 13:24:51 +0000

    Updated outputs

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "Hourly_bucket_name" {
+    value = module.s3_buckets.Hourly_bucket_name
+}
+output "Monthly_bucket_name" {
+   value = module.s3_buckets.Monthly_bucket_name
+}
+
+output "Weekly_bucket_name" {
+   value = module.s3_buckets.Weekly_bucket_name
+}
+
